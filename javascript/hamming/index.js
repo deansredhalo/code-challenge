1c70c1fbc03281ee255e941d4122bdaf0f2a5129
T-Jay Tipps <deansredhalo@gmail.com>
2017-08-13 23:55:53 -0500

    solve for js and py

--- javascript/hamming/index.js
+++ javascript/hamming/index.js
@@ -3,5 +3,26 @@
 // your code meets all of the conditions.
 // You may use ES6 or ES5 to solve.
 
+class Hamming {
+  static compute(str1, str2) {
+    let distance = 0;
+
+    if (str1.length !== str2.length) {
+      throw new Error('DNA strands must be of equal length.');
+    } else if (str1 !== str2) {
+      let i = 0;
+
+      while (i < str1.length) {
+        if (str1[i] !== str2[i]) {
+          distance++;
+        }
+        i++;
+      }
+
+    }
+
+    return distance;
+  }
+}
 
 module.exports = Hamming;
--- python/hamming/__init__.py
+++ python/hamming/__init__.py
@@ -3,5 +3,16 @@
 # unit tests pass and your code meets all of the conditions.
 #
 
-def compute():
-    pass
+def compute(str1, str2):
+  str1len = len(str1)
+  str2len = len(str2)
+  distance = 0
+
+  if str1len != str2len:
+    raise ValueError("DNA strands must be of equal length.")
+  elif str1 != str2:
+    for i in range(str1len):
+      if str1[i] != str2[i]:
+        distance += 1
+
+  return distance
